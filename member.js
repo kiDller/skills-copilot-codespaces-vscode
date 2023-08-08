afe88bf0f792b640e179eb94b1e54e5ac2eefd3e
kiDller <92949020+kiDller@users.noreply.github.com>
2023-08-08 12:03:54 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,15 +1,3 @@
-function skillsmember() {
-    var skills = document.getElementById("skills").value;
-    var skills = skills.split(",");
-    var skills = skills.map(function (item) {
-        return item.trim();
-    });
-    var skills = skills.filter(function (item) {
-        return item != "";
-    });
-    var skills = skills.map(function (item) {
-        return item.toUpperCase();
-    });
-    var skills = skills.sort();
-    document.getElementById("skills").value = skills;
-}
+function skillsMember( member ) {
+    console.log( member.name + " has skills: " + member.skills.join(", ") );
+}  
